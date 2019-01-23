8269d20376f551e6ae1598948a7fd79a21fdeb3b
Matt Burstein <j.matthew.burstein@gmail.com>
2019-01-23 23:18:31 +0000

    feat: add /items route

--- server/index.js
+++ server/index.js
@@ -4,4 +4,13 @@ const port = 5000
 
 app.get('/', (req, res) => res.send("Hello world!"))
 
+app.get('/items', (req, res) => {
+  res.json(
+    [
+      {name: "Baked beans", quantity: 1},
+      {name: "Toothepaste", quantity: 3}
+    ]
+  )
+})
+
 app.listen(port, () => console.log(`Listening on port ${port}`))
