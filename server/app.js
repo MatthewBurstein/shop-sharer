09d6ab10e6ef2f3dc0f62609a1fa4385d0a61a5a
Matt Burstein <j.matthew.burstein@gmail.com>
2019-02-03 14:41:34 +0000

    chore: update Dockerfile and app.js to use new packages

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 const app = require('express')()
 const cors = require('cors')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
 
 const port = 5000
 
 app.use(cors())
+
+app.use(logger('dev'))
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => res.send("Hello world!"))
 
