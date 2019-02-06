7555c9eba003218f0d47d13cd6862e84d5f4f9a9
Matt Burstein <j.matthew.burstein@gmail.com>
2019-02-06 20:39:36 +0000

    chore: add migration for items

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -1,25 +1,26 @@
 const dotenv = require('dotenv').config();
 
-module.exorts = {
-  "development": {
-    "username": process.env.POSTGRES_USER,
-    "password": process.env.POSTGRES_PASSWORD,
-    "database": process.env.POSTGERS_DB,
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+module.exports = {
+  development: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGERS_DB,
+    host: "db",
+    dialect: "postgres",
+    port: 5432
   },
-  // "test": {
-  //   "username": "root",
-  //   "password": null,
-  //   "database": "database_test",
-  //   "host": "127.0.0.1",
-  //   "dialect": "postgres"
+  // test: {
+  //   username: root,
+  //   password: null,
+  //   database: database_test,
+  //   host: 127.0.0.1,
+  //   dialect: postgres
   // },
-  // "production": {
-  //   "username": "root",
-  //   "password": null,
-  //   "database": "database_production",
-  //   "host": "127.0.0.1",
-  //   "dialect": "postgres"
+  // production: {
+  //   username: root,
+  //   password: null,
+  //   database: database_production,
+  //   host: 127.0.0.1,
+  //   dialect: postgres
   // }
 }
