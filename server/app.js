33b67a41305d06805947da85bc6daf70e80c1cd3
Matt Burstein <j.matthew.burstein@gmail.com>
2019-02-09 13:07:35 +0000

    feat: implemeent dbCLient.getAllItems

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const app = require('express')()
 const cors = require('cors')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
+const dbClient= require("./src/db/dbClient")
 
 const port = 5000
 
@@ -15,12 +16,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/', (req, res) => res.send("Hello world!"))
 
 app.get('/items', (req, res) => {
-  res.json(
-    [
-      {name: "Baked beans", quantity: 1},
-      {name: "Toothepaste", quantity: 3}
-    ]
-  )
+  dbClient.getAllItems()
+  .then(items => Promise.resolve(
+    items.map(item => {
+      return { item: item.name, quantity: item.quantity }
+    })
+  ))
+  .then(result => {
+    console.log(result);
+    res.json(result);
+  })
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
--- server/src/db/dbClient.js
+++ server/src/db/dbClient.js
@@ -1,16 +1,7 @@
-const Sequelize = require('sequelize');
-const database = process.env("POSTGRES_DB")
-const user = process.env("POSTGRES_USER")
-const password = process.env("POSTGRES_PASSWORD")
+const db = require("./models/index");
 
-const sequelize = new Sequelize(database, user, password, {
-  host: 'localhost',
-  dialect: 'postgres',
-  operatorsAliases: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-});
+module.exports = {
+  getAllItems: function()  {
+    return db.Item.findAll()
+  }
+}
